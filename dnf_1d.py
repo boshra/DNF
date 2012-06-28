28ce0de1d9380aeced0da125134c6325954b826c
Rober <rboshra@Inglip.(none)>
2012-06-28 10:09:54 -0300

    Several other implementations were added including the 1d DNF and path integration

---
+++ dnf_1d.py
@@ -0,0 +1,145 @@
+##########################################################
+##                                                      ##
+## Module: dnf_1d.py                                    ##
+##                                                      ##
+## Version: 0.1                                         ##
+##                                                      ##
+## Description: A running simulation of a dynamic       ##
+##  neural field with several modifications for         ##
+##  improving runtime. This is the 1d version. It       ##
+##  is used as a simple starting point for underst-     ##
+##  ing path integration along with a working           ##
+##  implementation of the mexican hat gaussians which   ##
+##  enable multiple hypothesese on a single neural      ##
+##  field                                               ##
+##                                                      ##
+## Checklist: 1- Enable multi bubbles          X        ##
+##            2- Enable path integration       X        ##
+##                                                      ##
+##########################################################
+
+
+from numpy import *
+from scipy.signal import *
+from mpl_toolkits.mplot3d import Axes3D
+from matplotlib import cm
+import matplotlib.pyplot as plt
+from matplotlib.ticker import LinearLocator, FormatStrFormatter
+
+
+class dnf:
+    ######### Simulation variables ############
+    
+    n=100               #Number of nodes per side
+    tau=0.1             #Tau
+    I=zeros((n,1))      #Input activity
+    dx=2*pi/n           #Length per node in the x dimension
+    sig=2*pi/10*0.6     #Sigma of the gaussian function
+    
+    
+    ###########################################
+    
+    #Takes a location between 0 and 2pi for both x and y along with the sigma
+    # value for the gaussian function. Produces an n*n array with the gaussian
+    # figure.
+    @staticmethod
+    def gauss_pbc(locx,sig):
+        w=zeros((dnf.n,1))
+        for i in range(dnf.n):
+                d=min([abs(i*dnf.dx-locx) , 2*pi-abs(i*dnf.dx-locx)]) 
+                w[i]=1./(sqrt(2*pi)*sig)\
+                    *exp(-(d**2/(2*sig**2)))
+                    
+ 
+                
+        return w   
+    
+    
+    
+    def __init__(self):
+        self.t = 0
+        self.gauss = dnf.gauss_pbc(pi,self.sig)
+        self.u=zeros((dnf.n,1))      #The neural field state at a specific time
+#        self.z = (1000*(self.hebbPI()-0.095) * 0.7 + 1000*(self.hebb()-0.095))/2
+        self.z = 1000*(self.hebb()-0.095)
+        self.diffgauss = dnf.gauss_pbc(pi/2,0.2) - dnf.gauss_pbc(pi/2,dnf.sig)
+        self.xall=[]
+    
+        
+    
+    def hebbPI(self):
+        w=zeros((self.n,self.n))
+        for i in range(self.n):
+            r=dnf.gauss_pbc(i*self.dx,self.sig)
+            r2=dnf.gauss_pbc((i+5)%self.n*self.dx,self.sig)
+            w=w+dot(r,r2.transpose())
+        return w/self.n
+    
+    
+    #Uses hebbian learning to produce a single n*n array of the relation between
+    # two gaussians. It yields the weights between the middle node and all the
+    # others.
+    def hebb(self):
+        w=zeros((self.n,self.n))
+        for i in range(self.n):
+            r=dnf.gauss_pbc(i*self.dx,self.sig)
+            w=w+dot(r,r.transpose())
+        return w/self.n
+    
+    def hebbMulti(self):
+        w=zeros((self.n,self.n))
+        for i in range(self.n):
+            r=dnf.gauss_pbc(i*self.dx,0.2) - dnf.gauss_pbc(i*self.dx,dnf.sig)
+            w=w+dot(r,r.transpose())
+        return w/self.n
+    
+    
+    #Takes a dynamic field state along with an activity input and the weight
+    # from hebb. It updates the dynamic neural field and returns it after one
+    # step in time. 
+    def update(self,I):
+        self.t += 1
+        r=0.5*(tanh(0.1*self.u)+1)
+        self.u=self.u+ self.tau*((-self.u)+dot(self.z,r)*self.dx+I)
+        x=0.5*(tanh(0.1*self.u)+1)
+        self.xall = self.xall + [list(self.u.reshape(-1,))]
+    
+    #Takes a dnf state and draws it in 3dwith respect to time.
+    def plot(self):
+        fig = plt.figure()
+        ax = fig.gca(projection='3d')
+        self.X, self.Y = meshgrid(
+            arange(self.n),
+            arange(self.t))      #A grid for plotting purposes        
+        surf = ax.plot_surface(self.X, self.Y, self.xall, cmap=cm.jet,
+            linewidth=0, antialiased=True)
+        ax.w_zaxis.set_major_locator(LinearLocator(10))
+        ax.w_zaxis.set_major_formatter(FormatStrFormatter('%.1f'))
+        ax.set_zlabel("Excitation")
+        ax.set_xlabel("Node 'X'")
+        ax.set_ylabel("Time")
+        fig.colorbar(surf, shrink=0.5, aspect=5)
+        plt.show()
+
+
+
+
+
+    
+#Test case for the module
+if __name__ == "__main__":
+    dnfex = dnf()
+    
+    #Provide input at pi/2 for 50 steps
+    I=dnf.gauss_pbc(pi/2,dnf.sig)
+    for t in arange(100):
+       dnfex.update(I)
+       
+       
+    I=zeros((dnf.n,1))
+    for I in arange(100):
+        dnfex.update(I)
+
+        
+
+    dnfex.plot()
---
+++ newtest.py
@@ -0,0 +1,99 @@
+"""
+Last modified: 7 June 2012
+
+"""
+
+
+from numpy import *
+from mpl_toolkits.mplot3d import Axes3D
+import matplotlib
+from matplotlib import cm
+import matplotlib.pyplot as plt
+from matplotlib.ticker import LinearLocator, FormatStrFormatter
+
+
+
+n=100
+tau=0.1
+u=zeros(n)
+I=zeros(n)
+dx=2*pi/n
+sig=2*pi/10*0.6
+
+
+def gauss_pbc(loc,sig):
+    x=zeros(n)
+    for i in range(n):
+        d=min([abs(i*dx-loc), 2*pi-abs(i*dx-loc)])
+        x[i]=1./(sqrt(2*pi)*sig)*exp(-d**2/(2*sig**2))
+    return x
+   
+def hebb():
+    w=zeros((n,n))
+    for i in range(n):
+        r=array([gauss_pbc(i*dx,sig)])
+        w=w+dot(r.transpose(),r)
+    return w/n
+   
+def update(u,I):
+    x=0.5*(tanh(0.1*u)+1)
+    u=u+tau*(-u+dot(w,x)*dx+I)
+    return u
+
+
+if __name__ == "__main__":
+    #Initialize weights
+    w=1000*(hebb()-0.095)
+    
+    #Initialize dnf state-history list    
+    xall=[]
+    
+    #Provide input at pi/2 for 50 steps
+    I=gauss_pbc(pi/2,sig)
+    for t in arange(50):
+        u=update(u,I)
+        x=0.5*(tanh(0.1*u)+1)
+        xall=xall+[x]
+
+    #Wait till it stabilizes for 50 steps
+    I=zeros(n)
+    for t in arange(50):
+        u=update(u,I)
+        x=0.5*(tanh(0.1*u)+1)
+        xall=xall+[x]
+    
+#    #Provide new input at 3pi/2 for 90 steps to overcome 
+#    #the belief of the previous state
+#    I=gauss_pbc(3 * pi / 2,sig)
+#    for t in arange(90):
+#        u=update(u,I)
+#        x=0.5*(tanh(0.1*u)+1)
+#        xall=xall+[x]
+
+    #Wait till it stabilizes for 50 steps
+    I=zeros(n)
+    for t in arange(200):
+        u=update(u,I)
+        x=0.5*(tanh(0.1*u)+1)
+        xall=xall+[x]
+        
+    #Create a figure for the 3d plot
+    fig = plt.figure()
+    ax = fig.gca(projection='3d')
+    X, Y = meshgrid(arange(n), arange(300))
+    surf = ax.plot_surface(X, Y, asarray(xall), cmap=cm.jet,
+        linewidth=0, antialiased=True)
+    ax.w_zaxis.set_major_locator(LinearLocator(10))
+    ax.w_zaxis.set_major_formatter(FormatStrFormatter('%.1f'))
+    ax.set_zlabel("Excitation")
+    ax.set_xlabel("Node")
+    ax.set_ylabel("Time")
+    fig.colorbar(surf, shrink=0.5, aspect=5)
+    plt.figure()
+    
+    #Create a weight image in a new figure
+    h = plt.imshow(w)
+    plt.show()
+    
+    
+    
